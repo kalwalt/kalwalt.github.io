38e3b98335ad2cb696efe0e5705866b6cfe7347c
kalwalt <info@kalwaltart.it>
2019-02-22 21:20:43 +0100

    transforming the img tag to amp-img

--- admin/src/amp-img-widget.js
+++ admin/src/amp-img-widget.js
@@ -101,7 +101,7 @@ CMS.registerEditorComponent({
       <figure>
         <h3>${obj.title || ''}</h3>
         <a href="${obj.link || ''}" rel="${obj.rel || ''}" target="${obj.target || ''}">
-        <img src="${obj.src}" alt="${obj.alt || ''}" width="${obj.width}" height="${obj.height}" />
+        <amp-img src="${obj.src}" alt="${obj.alt || ''}" width="${obj.width}" height="${obj.height}" /></amp-img>
         </a>
         <figcaption>
           <h4 class="${obj.className}">${obj.caption || ''}</h4>
