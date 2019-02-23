c42e5e2b09f7ebc157dea12d00ac9b0e144a0612
kalwalt <info@kalwaltart.it>
2019-02-23 17:48:05 +0100

    added webp and jpg ids

--- admin/src/amp-img-widget.js
+++ admin/src/amp-img-widget.js
@@ -1,57 +1,3 @@
-/*
-var CategoriesControl = createClass({
-handleChange: function(e) {
-this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
-},
-
-render: function() {
-var value = this.props.value;
-return h('input', { type: 'text', value: value ? value.join(', ') : '', onChange: this.handleChange });
-}
-});
-
-var CategoriesPreview = createClass({
-render: function() {
-  return h('ul', {},
-    this.props.value.map(function(val, index) {
-      return h('li', {key: index}, val);
-    })
-  );
-}
-});
-
-CMS.registerWidget('categories', CategoriesControl, CategoriesPreview);
-
-CMS.registerEditorComponent({
-// Internal id of the component
-id: "amp-img",
-// Visible label
-label: "Amp-img",
-// Fields the user need to fill out when adding an instance of the component
-fields: [
-      {label: "Image_id", name: "id", widget: "string"},
-      {label: "Image_name", name: "fileName", widget: "string"}
-    ],
-// Pattern to identify a block as being an instance of this component
-pattern: /^amp-img (\S+)$/,
-// Function to extract data elements from the regexp match
-fromBlock: function(match) {
-  return {
-    id: match[1]
-  };
-},
-// Function to create a text block from an instance of this component
-toBlock: function(obj) {
-  return ( 'amp-img ' + obj.id );
-},
-// Preview output for this component. Can either be a string or a React component
-// (component gives better render performance)
-toPreview: function(obj) {
-  return (
-    '<img width="1200px" height="859px" src="https://ucarecdn.com/' + obj.id +'/' + obj.fileName '"/>');
-}
-});
-*/
 CMS.registerEditorComponent({
   // Internal id of the component
   id: "figure",
@@ -59,7 +5,9 @@ CMS.registerEditorComponent({
   label: "Figure",
   // Fields the user need to fill out when adding an instance of the component
   fields: [
-    {name: 'src', label: 'Image', widget: 'string'},
+    {name: 'name', label: 'ImageName', widget: 'string'},
+    {name: 'webp_id', label: 'Webp_id', widget: 'string'},
+    {name: 'jpg_id', label: 'Jpg_id', widget: 'string'},
     {name: 'link', label: 'Link', widget: 'string'},
     {name: 'target', label: 'Target window', widget: 'string'},
     {name: 'rel', label: 'Rel (optional if link set)', widget: 'string'},
